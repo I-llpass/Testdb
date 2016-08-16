23905cc7f4128eacacf30b5e3159a7e7f4fb903a
I'llpass <gautamkhosla1111@gmail.com>
2016-08-16 11:20:58 +0530

    to comiiit

--- app/src/main/java/com/example/cynogen/test/MainActivity.java
+++ app/src/main/java/com/example/cynogen/test/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.cynogen.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.SQLOutput;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //help the project to push
+        btn= (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this,"this is toast",Toast.LENGTH_LONG).show();
     }
 }
